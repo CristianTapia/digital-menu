3a42c092348041df4ee72cc2d77fa2222cca618a
Cristian <ctapia.macaya@gmail.com>
2023-01-18 21:53:20 -0300

    last changes

--- js/cervezas.js
+++ js/cervezas.js
@@ -2,18 +2,16 @@
 -    Dropdown menu    -
 ---------------------*/
 
-/* When the user clicks on the button, 
-toggle between hiding and showing the dropdown content */
-function myFunction() {
-  document.getElementById("myDropdown").classList.toggle("show");
+// Show/hide dropdown
+function showOptions() {
+  document.getElementById("dropdown").classList.toggle("show");
 }
 
 // Close the dropdown if the user clicks outside of it
 window.onclick = function(event) {
   if (!event.target.matches('.dropbtn')) {
     var dropdowns = document.getElementsByClassName("dropdown-content");
-    var i;
-    for (i = 0; i < dropdowns.length; i++) {
+    for (var i = 0; i < dropdowns.length; i++) {
       var openDropdown = dropdowns[i];
       if (openDropdown.classList.contains('show')) {
         openDropdown.classList.remove('show');
@@ -46,28 +44,28 @@ function readMore() {
 -      Modal box      -
 ---------------------*/
 
-// Get the modal
-var modal = document.getElementById("myModal");
-
-// Get the button that opens the modal
-var btn = document.getElementById("myBtn");
-
-// Get the <span> element that closes the modal
-var span = document.getElementsByClassName("close")[0];
-
-// When the user clicks the button, open the modal 
+var modal = document.getElementById("modal-box");
+var btn = document.getElementById("modal-btn");
+var div = document.getElementById("close");
+// Open the modal 
 btn.onclick = function() {
   modal.style.display = "block";
 }
-
-// When the user clicks on <span> (x), close the modal
-span.onclick = function() {
+// Close the modal
+div.onclick = function() {
   modal.style.display = "none";
 }
 
-// When the user clicks anywhere outside of the modal, close it
 // window.onclick = function(event) {
 //   if (event.target == modal) {
 //     modal.style.display = "none";
 //   }
 // }
+
+var closeModal = function(event) {
+  if (event.target == modal) {
+        modal.style.display = "none";
+  }
+}
+
+window.addEventListener('click', closeModal);
