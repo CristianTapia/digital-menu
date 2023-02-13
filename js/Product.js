0cb8251428439ecb62ab4deb6e2a2099542e7054
Cristian <ctapia.macaya@gmail.com>
2023-02-13 17:38:02 -0300

    Cart working, POO initiated

---
+++ js/Product.js
@@ -0,0 +1,13 @@
+class Product {
+    constructor(name, information, price, picture, procedence) {
+        this.name = name;
+        this.information = information;
+        this.price = price;
+        this.procedence = procedence;
+        this.picture = picture;
+    }
+
+    setProductToMenu() {
+    
+    }
+}
