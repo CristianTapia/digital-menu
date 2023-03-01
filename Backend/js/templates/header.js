05b68248c0be58561544b47458faa933692ce0c7
Cristian <ctapia.macaya@gmail.com>
2023-03-01 17:06:30 -0300

    header changes

--- Backend/js/templates/header.js
+++ Backend/js/templates/header.js
@@ -1,19 +1,19 @@
 export { headerTemplate }
 
 let headerTemplate = `
-<header class="p-3 text-bg-dark">
-    <div class="container">
+<header class="p-3 bg-dark">
+    <div class="container-fluid">
       <div class="d-flex flex-wrap align-items-center justify-content-center justify-content-lg-start">
         <a href="/" class="d-flex align-items-center mb-2 mb-lg-0 text-white text-decoration-none">
-          <svg class="bi me-2" width="40" height="32" role="img" aria-label="Bootstrap"><use xlink:href="#bootstrap"></use></svg>
+          <img src="/img/logo.jpg" alt="" width=50" height="50" class="rounded-circle me-2">
         </a>
 
         <ul class="nav col-12 col-lg-auto me-lg-auto mb-2 justify-content-center mb-md-0">
-          <li><a href="#" class="nav-link px-2 text-secondary">Home</a></li>
+          <li><a href="#" class="nav-link px-2 text-white start-0">Schopería 4 1/2</a></li>
         </ul>
 
         <div class="text-end">
-          <button type="button" class="btn btn-outline-light me-2">Login</button>
+          <button type="button" class="btn btn-outline-light me-2">Cerrar sesión</button>
         </div>
       </div>
     </div>
