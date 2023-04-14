fb88cc2112a73f31f09787576aa2b205e0e236ea
Cristian <ctapia.macaya@gmail.com>
2023-04-13 21:18:12 -0400

    Sending data to the API from stactic JS

--- public/add-waiter.js
+++ public/add-waiter.js
@@ -2,8 +2,26 @@ let waiterForm = document.getElementById("add-waiter");
 
 waiterForm.addEventListener("submit", (event) => {
   event.preventDefault();
-  let name = waiterForm.elements["name"].value;
-  let lastName = waiterForm.elements["last-name"].value;
-  console.log("name", name);
-  console.log("last name", lastName);
+  let inputName = waiterForm.elements["name"].value;
+  let inputLastName = waiterForm.elements["last-name"].value;
+  const data = { name: inputName, lastName: inputLastName };
+  
+  async function postJSON(data) {
+    try {
+      const response = await fetch("http://localhost:3000/waiters", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(data),
+      });
+  
+      const result = await response.json();
+      console.log("Success: ", result);
+    } catch (error) {
+      console.error("Error: ", error);
+    }
+  }
+  
+  postJSON(data);
 });
